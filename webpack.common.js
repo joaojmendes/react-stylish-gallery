d6c1676560fabbbdba6cbb050c760c80e1a5cd2c
Nichita <nichita.sindie@yaho.com>
2017-09-18 15:03:53 +0100

    fixing images

--- webpack.common.js
+++ webpack.common.js
@@ -13,6 +13,7 @@ module.exports = {
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    publicPath: path.resolve(__dirname, 'dist'),
   },
   // Plugins
   plugins: [
